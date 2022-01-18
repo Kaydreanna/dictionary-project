f93787631b7481056a331dd979471fa2ae920453
Kaydreanna <94328465+Kaydreanna@users.noreply.github.com>
2022-01-17 18:04:15 -0700

    Added Search Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kaydreanna Castaneda</small>
+        </footer>
+      </div>
     </div>
   );
 }
