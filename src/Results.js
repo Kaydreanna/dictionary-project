82eddda02abd2e4a481f0bd18c486b930a73b544
Kaydreanna <94328465+Kaydreanna@users.noreply.github.com>
2022-02-08 19:53:31 -0700

    Still Debugging

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,29 @@
 import React from "react";
-//import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
+import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h1>Hello from Results</h1>
+        <section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <section key={index}>
+              <Meaning meaning={meaning} />
+            </section>
+          );
+        })}
       </div>
     );
   } else {
